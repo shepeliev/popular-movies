56f752ed8b24cb784f539bedff0b79b6de3219d8
Oleksandr Shepeliev <a.shepeliev@gmail.com>
2016-11-08 00:06:31 +0200

    #3 sort movies preference summary should be updated on the setting changes

--- app/src/main/java/com/shepeliev/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/shepeliev/popularmovies/SettingsActivity.java
@@ -1,24 +1,43 @@
 package com.shepeliev.popularmovies;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
 public class SettingsActivity extends PreferenceActivity {
+
+  private static final Preference.OnPreferenceChangeListener sPreferenceChangeListener =
+      new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+          String stringValue = newValue.toString();
+          if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int index = listPreference.findIndexOfValue(stringValue);
+            preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
+          } else {
+            preference.setSummary(stringValue);
+          }
+
+          return true;
+        }
+      };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
     addPreferencesFromResource(R.xml.preferences);
-
-    updateSummaries();
+    bindValueToSummary(findPreference(getString(R.string.pref_sort_key)));
   }
 
-  private void updateSummaries() {
-    ListPreference listPreference =
-        (ListPreference) findPreference(getString(R.string.pref_sort_key));
-    listPreference.setSummary(listPreference.getEntry());
+  private void bindValueToSummary(Preference preference) {
+    preference.setOnPreferenceChangeListener(sPreferenceChangeListener);
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+    String value = preferences.getString(preference.getKey(), "");
+    sPreferenceChangeListener.onPreferenceChange(preference, value);
   }
 }
